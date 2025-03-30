52bcbd47f403bc5981a9758320985f7d3826a556
George Hornseth <gbhornseth@gmail.com>
2025-03-30 02:24:33 -0500

    Started working on code writer.

--- vm_2_hack.py
+++ vm_2_hack.py
@@ -16,7 +16,39 @@ def parse(file_path):
     remove_whitespace_and_labels()
 
     return lines_new
- 
-lines = parse("SimpleAdd.vm")
 
-print(lines)
+
+def code_writer(lines):
+
+    hack_code = []
+
+    for i in lines:
+        words = i.split()
+        if words[0] == "push":
+            if words[1] == "constant":
+                hack_code.append(f"@{words[2]}")
+                hack_code.append("D=A")
+                hack_code.append("@SP")
+                hack_code.append("A=M")
+                hack_code.append("M=D")
+                hack_code.append("@SP")
+                hack_code.append("M=M+1")
+        elif words[0] == "add":
+            hack_code.append("@SP")
+            hack_code.append("AM=M-1")
+            hack_code.append("D=M")
+            hack_code.append("A=A-1")
+            hack_code.append("M=M+D")
+        
+    return hack_code
+
+
+def vm_2_hack():
+    lines = parse("SimpleAdd.vm")
+    final_line = code_writer(lines)
+
+    for i in final_line:
+        print(i)
+
+
+vm_2_hack()
